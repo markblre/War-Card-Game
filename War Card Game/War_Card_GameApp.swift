cae5cd6084c493c197ff5e6caadf0e2c2192fa70
Mark Ballereau <mark.b@live.fr>
2023-06-29 14:22:57 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Mark Ballereau on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
